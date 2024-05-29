5a74ee02db1b037ce519da99d1f83a51f079e38f
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-05-29 16:57:41 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import "./App.css";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Home from "./pages/Home";
+function App() {
+  
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
